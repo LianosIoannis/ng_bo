6183b784f4e6b112309e8736b4f023bb61735124
jlianos <J.lianos@futuresoft.gr>
2026-07-24 16:58:50 +0300

    feat: implement responsive menu with loading state and error handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from "@angular/core";
-import { FormTest } from "./tests/form-test/form-test";
-import { TableTest } from "./tests/table-test/table-test";
+import { Component, inject, signal } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { catchError, of } from "rxjs";
+import { Menu } from "./libraries/menu/menu";
+import { MenuLoader } from "./services/menu-loader";
 
 @Component({
 	selector: "app-root",
-	imports: [FormTest, TableTest],
+	imports: [Menu, FaIconComponent],
 	templateUrl: "./app.html",
+	host: {
+		"(keydown.escape)": "closeMenu()",
+	},
 })
 export class App {
-	protected readonly title = signal("ng_bo");
+	private readonly menuLoader = inject(MenuLoader);
+
+	protected readonly menuIcon = faBars;
+	protected readonly menuOpen = signal(false);
+	protected readonly menuData = toSignal(this.menuLoader.loadMenu().pipe(catchError(() => of(null))), {
+		initialValue: undefined,
+	});
+
+	protected openMenu() {
+		this.menuOpen.set(true);
+	}
+
+	protected closeMenu() {
+		if (!this.menuOpen()) {
+			return;
+		}
+
+		this.menuOpen.set(false);
+	}
 }
